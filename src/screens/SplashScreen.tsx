4d39555048418a85605c49423b7402f2c81a0bb1
najeebabdi <najeebwarsame@gmail.com>
2025-02-12 16:41:33 +0200

    Splash Screen update

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
 import { View, Image, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type RootStackParamList = {
+  Splash: undefined;
+  Login: undefined;
+};
+
+type SplashScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Splash'>;
 
 const SplashScreen = () => {
-    const navigation = useNavigation();
+    const navigation = useNavigation<SplashScreenNavigationProp>();
 
     useEffect(() => {
         setTimeout(() => {
             navigation.replace('Login'); // Navigate to Login Screen
-        }, 3000); // 3-second delay
-    }, [navigation]); // Add navigation as dependency
+        }, 3000);
+    }, [navigation]);
 
     return (
         <View style={styles.container}>
@@ -23,10 +31,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#313CA1', // Matches website color scheme
+        backgroundColor: '#313CA1',
     },
     logo: {
-        width: 150, // Adjust as needed
+        width: 150,
         height: 150,
         resizeMode: 'contain',
     },
