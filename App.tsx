522c26cd7e7d472cba9e3fa2c3b36ea6aed9082f
najeebabdi <najeebwarsame@gmail.com>
2025-02-13 19:29:48 +0200

    Your commit message here

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
 
+
 // Create our stack navigator
 const Stack = createStackNavigator();
 
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SplashScreen from '../src/screens/SplashScreen';
-import LoginScreen from '../src/screens/LoginScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SplashScreen from '../screens/SplashScreen';
+
 const Stack = createStackNavigator();
 
 export default function App() {
