d5058db3edeb48f274332bb3971bcf725b35cc11
najeebabdi <najeebwarsame@gmail.com>
2025-03-06 18:27:12 +0200

    babel config update

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         root: ['./src'],
         alias: {
-          screens: './src/screens'
+          screens: './src/screens',
         },
       },
     ],
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = {
+  resolver: {
+    extraNodeModules: {
+      'babel-plugin-module-resolver': path.resolve(__dirname, 'node_modules/babel-plugin-module-resolver'),
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
